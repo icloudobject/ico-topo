699cff1c607ad2b58c917587fe11fad7b9f95f14
icloudobject <icloudobject@users.noreply.github.com>
2014-12-20 18:13:24 -0800

    init setup

---
+++ client/icotopo/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'ICloudObject data center topology',
+    'author': 'ICloudObject',
+    'url': 'http://github.com/icloudobject/ico-topo',
+    'download_url': 'http://github.com/icloudobject/ico-topo',
+    'author_email': 'icloudobject@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['icotopo'],
+    'scripts': [],
+    'name': 'icotopo'
+}
+
+setup(**config)
