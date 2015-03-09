f842154e089372476a3049bbeb6f6ed0bef261f8
icloudobject <icloudobject@users.noreply.github.com>
2015-03-08 19:46:16 -0700

    adding id attr when creating the object

--- server/topo/src/main/java/com/icloudobject/topo/resource/EntityResource.java
+++ server/topo/src/main/java/com/icloudobject/topo/resource/EntityResource.java
@@ -205,7 +205,8 @@ public class EntityResource extends
                 e.printStackTrace();
             }
         } else {
-            payload.put("_oid", objectId);
+        	payload.put("_oid", objectId);
+        	payload.put("id", objectId);
             try {
                 Response r = super.createEntity(uriInfo, reponame, branch,
                         className, priority, consistPolicy, payload.toString(),
